bd3bddb595cd90224bb3b7e520292b7823917b90
Geun Woo Park <parklaus1078@gmail.com>
2024-12-04 13:21:29 +0900

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -93,7 +93,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+            Change applied
         </a>
       </footer>
     </div>
